41ad4be778cf943b2e8875a856e67620d63fb9ed
sajjadhaider <sajjadkazmi962@gmail.com>
2025-02-05 20:57:28 +0500

    cors issue fixed

--- src/main/java/com/spring3/oauth/jwt/controllers/UserController.java
+++ src/main/java/com/spring3/oauth/jwt/controllers/UserController.java
@@ -27,7 +27,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:8841")
 @RestController
 @RequestMapping("/api/v1")
 public class UserController {
