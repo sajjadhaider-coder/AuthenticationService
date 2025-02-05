983fa62bbcf6cb2b1623be5f7e7652bd59af15be
sajjadhaider <sajjadkazmi962@gmail.com>
2025-02-05 20:20:02 +0500

    cors issue fixed

--- src/main/java/com/spring3/oauth/jwt/configurations/CustomCorsConfig.java
+++ src/main/java/com/spring3/oauth/jwt/configurations/CustomCorsConfig.java
@@ -10,8 +10,8 @@ public class CustomCorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Apply to all endpoints
-                .allowedOrigins("localhost") // Specify allowed origins
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Specify allowed HTTP methods
+                .allowedOriginPatterns("*") // Specify allowed origins
+                .allowedMethods("*") // Specify allowed HTTP methods
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(true); // Allow credentials (e.g., cookies, authorization headers)
     }
