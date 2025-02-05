b8ef48342d84228e5fc0d13f9c585ce4335c6500
sajjadhaider <sajjadkazmi962@gmail.com>
2025-02-05 11:48:17 +0500

    Fixed .gitignore to track src/main

--- src/main/java/com/spring3/oauth/jwt/exceptions/InvalidCredentialsException.java
+++
@@ -1,7 +0,0 @@
-package com.spring3.oauth.jwt.exceptions;
-
-public class InvalidCredentialsException extends RuntimeException {
-    public InvalidCredentialsException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/spring3/oauth/jwt/exceptions/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.spring3.oauth.jwt.exceptions;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
-
-
